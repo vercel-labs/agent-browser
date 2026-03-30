a9a73b388e34a2aa3880107dd134a70a5d61e56a
Trevin Chow <trevin@trevinchow.com>
2026-03-29 22:24:52 -0700

    style: apply cargo fmt

--- cli/src/native/actions.rs
+++ cli/src/native/actions.rs
@@ -8927,7 +8927,10 @@ mod tests {
 
     #[test]
     fn test_glob_to_regex_exact_match() {
-        assert_eq!(super::glob_to_regex("https://example.com"), "^https://example\\.com$");
+        assert_eq!(
+            super::glob_to_regex("https://example.com"),
+            "^https://example\\.com$"
+        );
     }
 
     #[test]
